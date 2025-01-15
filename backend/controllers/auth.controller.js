d596cbf79ead3bd870f62843a9073b9aa810263c
Chris Troutner <chris.troutner@gmail.com>
2025-01-15 08:58:39 -0700

    Replacing default hero image with dectur island

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -54,6 +54,7 @@ export const signup = async (req, res) => {
 		}
 	} catch (error) {
 		console.log("Error in signup controller", error.message);
+		console.log(error)
 		res.status(500).json({ error: "Internal Server Error" });
 	}
 };
--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import XSvg from "../../../components/svgs/X";
+import DecaturImage from "../../../img/decatur.png";
 
 import { MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
@@ -56,47 +57,62 @@ const LoginPage = () => {
 	};
 
 	return (
-		<div className='max-w-screen-xl mx-auto flex h-screen'>
-			<div className='flex-1 hidden lg:flex items-center  justify-center'>
-				<XSvg className='lg:w-2/3 fill-white' />
+		<div>
+			<div className='items-center  justify-center'>
+				<br /><br />
+				<div>
+					<h1 className='text-4xl font-extrabold text-white' style={{textAlign: 'center'}}>Decatur Island Community Exchange</h1>
+				</div>
+				<br /><br />
+				<div>
+					<h2 className='text-4xl font-extrabold text-white' style={{textAlign: 'center'}}>DICE</h2>
+				</div>
 			</div>
-			<div className='flex-1 flex flex-col justify-center items-center'>
-				<form className='flex gap-4 flex-col' onSubmit={handleSubmit}>
-					<XSvg className='w-24 lg:hidden fill-white' />
-					<h1 className='text-4xl font-extrabold text-white'>{"Let's"} go.</h1>
-					<label className='input input-bordered rounded flex items-center gap-2'>
-						<MdOutlineMail />
-						<input
-							type='text'
-							className='grow'
-							placeholder='username'
-							name='username'
-							onChange={handleInputChange}
-							value={formData.username}
-						/>
-					</label>
+			
+			<div className='max-w-screen-xl mx-auto flex h-screen'>
+				<div className='flex-1 hidden lg:flex items-center  justify-center'>
+					<img src={DecaturImage} alt='Decatur Makers' className='w-2/3' />
+				</div>
+				<div className='flex-1 flex flex-col justify-center items-center'>
+					<form className='flex gap-4 flex-col' onSubmit={handleSubmit}>
+						<XSvg className='w-24 lg:hidden fill-white' />
+						<h1 className='text-4xl font-extrabold text-white'>{"Let's"} go.</h1>
+						<label className='input input-bordered rounded flex items-center gap-2'>
+							<MdOutlineMail />
+							<input
+								type='text'
+								className='grow'
+								placeholder='username'
+								name='username'
+								onChange={handleInputChange}
+								value={formData.username}
+							/>
+						</label>
 
-					<label className='input input-bordered rounded flex items-center gap-2'>
-						<MdPassword />
-						<input
-							type='password'
-							className='grow'
-							placeholder='Password'
-							name='password'
-							onChange={handleInputChange}
-							value={formData.password}
-						/>
-					</label>
-					<button className='btn rounded-full btn-primary text-white'>
-						{isPending ? "Loading..." : "Login"}
-					</button>
-					{isError && <p className='text-red-500'>{error.message}</p>}
-				</form>
-				<div className='flex flex-col gap-2 mt-4'>
-					<p className='text-white text-lg'>{"Don't"} have an account?</p>
-					<Link to='/signup'>
-						<button className='btn rounded-full btn-primary text-white btn-outline w-full'>Sign up</button>
-					</Link>
+						<label className='input input-bordered rounded flex items-center gap-2'>
+							<MdPassword />
+							<input
+								type='password'
+								className='grow'
+								placeholder='Password'
+								name='password'
+								onChange={handleInputChange}
+								value={formData.password}
+							/>
+						</label>
+						<button className='btn rounded-full btn-primary text-white'>
+							{isPending ? "Loading..." : "Login"}
+						</button>
+						{isError && <p className='text-red-500'>{error.message}</p>}
+					</form>
+					{/* 
+					<div className='flex flex-col gap-2 mt-4'>
+						<p className='text-white text-lg'>{"Don't"} have an account?</p>
+						<Link to='/signup'>
+							<button className='btn rounded-full btn-primary text-white btn-outline w-full'>Sign up</button>
+						</Link>
+					</div>
+					*/}
 				</div>
 			</div>
 		</div>
